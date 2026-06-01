b2862cc714bbf516563c349cec69833f2ce027f3
Daniel Zapata <42874951+DanielZR30@users.noreply.github.com>
2026-05-31 19:07:34 -0500

    fix: remove WebSockets/Sockets/Networking deps from Build.cs (NATS no longer needed)

--- Simulation/Source/Simulation/Simulation.Build.cs
+++ Simulation/Source/Simulation/Simulation.Build.cs
@@ -8,7 +8,7 @@ public class Simulation : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "WebSockets", "Json", "JsonUtilities", "Sockets", "Networking", "HTTP" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Json", "JsonUtilities", "HTTP" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
